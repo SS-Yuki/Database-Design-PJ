3cdddcfb72bc50ff670331b0e2641ba1b58c6bc9
Jiadi Su <20302010043@fudan.edu.cn>
2022-11-27 22:50:14 +0800

    entity sql

---
+++ repository.sql
@@ -0,0 +1,32 @@
+CREATE TABLE IF NOT EXISTS IssueInstance (	inst_id INT UNSIGNED,
+                                              case_id INT(10) UNSIGNED NOT NULL,
+    commit_id INT(10) UNSIGNED NOT NULL,
+    type INT(10) UNSIGNED,
+    status INT(10) UNSIGNED NOT NULL,
+    file_name VARCHAR(10)
+    , primary key(inst_id) );
+CREATE TABLE IF NOT EXISTS Commit (	commit_id INT UNSIGNED,
+                                       branch_id INT(10) UNSIGNED,
+    commiter VARCHAR(10),
+    time DATETIME,
+    hash VARCHAR(10)
+    , primary key(commit_id) );
+CREATE TABLE IF NOT EXISTS IssueLocation (	inst_id INT(10) UNSIGNED,
+    `order` INT(10) UNSIGNED,
+    start_line INT(10) UNSIGNED,
+    end_line INT(10) UNSIGNED,
+    start_col INT(10) UNSIGNED,
+    end_col INT(10) UNSIGNED
+    );
+CREATE TABLE IF NOT EXISTS File (	file_id INT UNSIGNED,
+                                     name VARCHAR(10),
+    path VARCHAR(10)
+    , primary key(file_id) );
+CREATE TABLE IF NOT EXISTS Branch (	branch_id INT UNSIGNED,
+                                       repository_id INT(10) UNSIGNED,
+    name VARCHAR(10)
+    , primary key(branch_id) );
+CREATE TABLE IF NOT EXISTS Repository (	repository_id INT UNSIGNED,
+                                           name VARCHAR(10),
+    url VARCHAR(10)
+    , primary key(repository_id) );
